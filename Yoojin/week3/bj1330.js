cdba45a1f243426a634109cb4dd0bea066550571
hnyoojin <0dbwls21@gmail.com>
2024-10-10 12:50:16 +0900

    백준 2단계 두 번째 문제 9498번 풀이 추가

--- Yoojin/week3/bj1330.js
+++ Yoojin/week3/bj1330.js
@@ -1,5 +1,6 @@
 let fs = require('fs');
-let input = fs.readFileSync('/dev/stdin').toString().split(' ');
+// let input = fs.readFileSync('/dev/stdin').toString().split(' ');
+let input = fs.readFileSync('./1330.txt').toString().split(' ');
 
 let A = parseInt(input[0]);
 let B = parseInt(input[1]);
