0fdd92ea813bc3bd6cd92c444293a5986ff577f5
hnyoojin <0dbwls21@gmail.com>
2024-10-10 14:21:10 +0900

    백준 2단계 풀이 완료

--- Yoojin/week3/bj2480.js
+++ Yoojin/week3/bj2480.js
@@ -8,17 +8,13 @@ let dice3 = parseInt(input[2]);
 
 let price = 0;
 
-if (dice1==dice2) {
-    if (dice2==dice3) 
-        price = 10000+dice1*1000;
-    if (dice2!=dice3)
-        price = 1000+dice1*100
-}
-if (dice1!=dice2) {
-    if (dice2==dice3) 
-        price = 1000+dice2*100;
-    if (dice2!=dice3)
-        price = 100*Math.max(Math.max(dice1,dice2), Math.max(dice2,dice3));
-}
+if (dice1===dice2 && dice2===dice3)
+    price = 10000+dice1*1000;
+else if (dice1===dice2 || dice1===dice3)
+    price = 1000+dice1*100;
+else if (dice1===dice2 || dice2===dice3)
+    price = 1000+dice2*100;
+else 
+    price = 100*Math.max(Math.max(dice1,dice2), Math.max(dice2,dice3));
 
-console.log("price = "+price);
+console.log(price);
