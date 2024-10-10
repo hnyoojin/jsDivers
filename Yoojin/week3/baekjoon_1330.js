eda32eef0523cd8e6c854ffe619eff7d1cf7c52e
hnyoojin <0dbwls21@gmail.com>
2024-10-10 12:22:13 +0900

    백준 2단계 첫문제 1330번 풀이 추가

--- Yoojin/week3/baekjoon_1330.js
+++ Yoojin/week3/baekjoon_1330.js
@@ -1,13 +1,12 @@
 let fs = require('fs');
-// let input = fs.readFileSync('/dev/stdin').toString().split(' ');
-let input = fs.readFileSync('./1330.txt').toString().split(' ');
+let input = fs.readFileSync('/dev/stdin').toString().split(' ');
 
-let A = input[0];
-let B = input[1];
+let A = parseInt(input[0]);
+let B = parseInt(input[1]);
 
 if (A>B)
     console.log('>');
-if (A<B)
+else if (A<B)
     console.log('<');
-if (A==B)
+else if (A==B)
     console.log('==');
